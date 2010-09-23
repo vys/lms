6fd0c04cd63f840eb72adbbf6fe65921172e9a58
Vinay Y S <vinay@vys.in>
2010-09-23 14:06:22 -0400

    Initial version.
    Signed-off-by: Vinay Y S <vinay@vys.in>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lms::Application.config.secret_token = '39a3a791f089e126d0475670c7ba65a27842ba47d7b216da169b333e51b04da73152e6fd81ee44c4eb1910f7fd74b54dbd78f25ff3e45c0ea10d1eef7a3c447f'
